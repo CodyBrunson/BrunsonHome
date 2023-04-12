98c0b4be12bf907ce66c3de7fba50c0ce191a569
CodyBrunson <55360215+CodyBrunson@users.noreply.github.com>
2023-04-12 17:36:40 -0500

    Crud final push
    Crud final push

---
+++ BrunsonHome.API/Migrations/20230412174234_HorseEntityUpdate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrunsonHome.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class HorseEntityUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PurchaseDate",
+                table: "Horses",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseDate",
+                table: "Horses");
+        }
+    }
+}
---
+++ BrunsonHome.API/Scripts/InitialCreate.sql
@@ -0,0 +1,43 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Horses] (
+    [Id] int NOT NULL IDENTITY,
+    [BarnName] nvarchar(max) NOT NULL,
+    [RegisteredName] nvarchar(max) NULL,
+    [AddDate] datetime2 NOT NULL,
+    [UpdateDate] datetime2 NULL,
+    CONSTRAINT [PK_Horses] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230412172742_InitialCreate', N'7.0.3');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Horses] ADD [PurchaseDate] datetime2 NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230412174234_HorseEntityUpdate', N'7.0.3');
+GO
+
+COMMIT;
+GO
+
